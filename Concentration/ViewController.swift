c5e72af29d47f684c836de0be3c240ec7df3b03c
Tarang <tsgoten@gmail.com>
2018-11-20 12:29:22 -0500

    comments with the first couple methods

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // the @IBAction puts the little circle on the left. XCode, not part of Swift
+    // func is the keyword, func can be outside of the class as well
+    // () has the list of arguments
+    // type is given by :UIButton
+    // Every argument has a name with it; An external name and an internal name
+    // if it had -> Int it means it returns an int
+    @IBAction func touchCard(_ sender: UIButton) {
+    
     }
-
-
+    
+    // external is withEmoji, and internal is emoji
+    // Number one requirement for names is the function should read like English
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        
+    }
+    
+    
 }
 
