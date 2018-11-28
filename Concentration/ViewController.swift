7137b241554126c1387ba60225c166d850d50448
Tarang <tsgoten@gmail.com>
2018-11-28 12:28:38 -0500

    fixed bug with second card

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -17,13 +17,24 @@ class ViewController: UIViewController {
     // Every argument has a name with it; An external name and an internal name
     // if it had -> Int it means it returns an int
     @IBAction func touchCard(_ sender: UIButton) {
-    
+        flipCard(withEmoji: "👻", on: sender)
     }
     
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
     // external is withEmoji, and internal is emoji
+    // If there is only one name then it is for both external and internal
     // Number one requirement for names is the function should read like English
     func flipCard(withEmoji emoji: String, on button: UIButton) {
-        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.9568627451, alpha: 1)
+        }
     }
     
     
